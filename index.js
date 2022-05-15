76875bc4dd016eedb5b70cc8f2bc13aff79087bd
Glebka90 <92037384+Glebka90@users.noreply.github.com>
2022-05-15 22:47:56 +0300

    Решиение
    Данное решение верно?

---
+++ index.js
@@ -0,0 +1,15 @@
+function f15() {
+   let out_15 = document.querySelector('.out-15');
+   let s_151 = document.querySelector('.s-151').value;
+   let s_152 = document.querySelector('.s-152').value;
+   let s_153 = document.querySelector('.s-153').value;
+   switch (s_153) {
+      case '&&':
+         out_15.textContent = +s_151 && +s_152;
+      case '||':
+         out_15.textContent = +s_151 || +s_152;
+
+   }
+}
+
+document.querySelector('.b-15').onclick = f15;
